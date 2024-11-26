ca4e35b54d92dc577c261d59da46adb43555a68c
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-11-26 16:02:57 +0900

    세팅 완료

--- 11720/11720.cpp
+++ 11720/11720.cpp
@@ -1,2 +1,20 @@
 // 숫자의 합
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N = 0;
+    string nums;
+    cin >> N;
+    cin >> nums;
+
+    int sum = 0;
+    for (int i = 0; i < nums.length(); i++)
+    {
+        sum += nums[i] -'0';
+    }
+    
+    cout << sum << "\n";
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Algorithm_JWT
  
  알고리즘 코딩 테스트 공부
+
+실행: Ctrl + R (Mac OS)
