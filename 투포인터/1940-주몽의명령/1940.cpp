48860aadd7a6b415ce43e6d13afdc77c7b2c0368
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-12-03 17:43:36 +0900

    1940 완료

--- 투포인터/1940-주몽의명령/1940.cpp
+++ 투포인터/1940-주몽의명령/1940.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -10,5 +11,38 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int N, M;
+    cin >> N >> M;
+    vector<int> array(N,0);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> array[i];
+    }
+
+    sort(array.begin(), array.end()); // 입력받은 수를 정렬
+
+    int count = 0;
+    int start = 0;
+    int end = N - 1;
     
+    while (start < end)
+    {
+        if (array[start] + array[end] < M)
+        {
+            start++;
+        }
+        else if (array[start] + array[end] > M)
+        {
+            end--;
+        }
+        else if (array[start] + array[end] == M)
+        {
+            count++;
+            start++;
+            end--;
+        }
+    }
+
+    cout << count << "\n";
 }
