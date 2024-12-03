d68d3fcfdf0d648ac82831f7c2457be93e581ad7
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-12-03 15:50:04 +0900

    분류화

---
+++ 투포인터/1940-주몽의명령/1940.cpp
@@ -0,0 +1,14 @@
+//주몽의 명령(투 포인터)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    
+}
