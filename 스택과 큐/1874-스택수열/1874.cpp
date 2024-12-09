ff968dff6d29d6f2145ce75b5a6d9005569be1a2
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-12-09 22:33:15 +0900

    Create 1874.cpp

---
+++ 스택과 큐/1874-스택수열/1874.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    vector<int> A(N, 0);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+
+}
