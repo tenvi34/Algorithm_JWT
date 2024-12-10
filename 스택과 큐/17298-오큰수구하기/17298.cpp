7943bc581d91f348596f939c25348f5d5be315ac
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-12-10 15:48:14 +0900

    Create 17298.cpp

---
+++ 스택과 큐/17298-오큰수구하기/17298.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    
+}
