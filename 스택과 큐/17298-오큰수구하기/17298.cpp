4a25ad2d8ed124e9ba3a7820c59c6f9085406138
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-12-13 16:26:18 +0900

    오큰수 문제 해결

--- 스택과 큐/17298-오큰수구하기/17298.cpp
+++ 스택과 큐/17298-오큰수구하기/17298.cpp
@@ -9,5 +9,31 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int N;
+    cin >> N;
+    vector<int> A(N); // 배열 생성
+    vector<int> result(N, -1); // 결과 배열
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    stack<int> stack; // 스택 생성
     
+    for (int i = 0; i < N; i++)
+    {
+        // 현재 숫자가 스택의 인덱스에 해당하는 숫자보다 큰 경우
+        while(!stack.empty() && A[stack.top()] < A[i])
+        {
+            result[stack.top()] = A[i]; // 현재 숫자가 오큰수
+            stack.pop();
+        }
+        stack.push(i);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << result[i] << " ";
+    }
 }
